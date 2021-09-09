6484b801ecb6e00319dffe9f143b5fc9830ec40e
Masumo Mwenyi <masumo.mwenyi@my.simpson.edu>
2021-09-09 14:30:49 -0500

    Lab one submission

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,5 @@
-print("Hi! This is a double quote mark: " We use it  printing.")
+print("Hello everyone, I encountered a problem with my fist lab. \nIt sucks very bad!")
+print("Dispite this problem, I did not give up on anything and I kep trying and asking for help")
+print("Now i am in the process of working on it again and I think i'm figuring it out.")
+print("this is the fun of codding right? It's going to be a long year \\ I think" )
+print("Let me know how \nyour year, is going ")
