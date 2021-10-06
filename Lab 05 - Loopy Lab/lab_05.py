d3bb3d8b76046239b37903198f26687916d089d6
Masumo Mwenyi <masumo.mwenyi@my.simpson.edu>
2021-10-06 18:47:53 -0500

    Finished lab 5

--- Lab 05 - Loopy Lab/lab_05.py
+++ Lab 05 - Loopy Lab/lab_05.py
@@ -14,88 +14,73 @@ def draw_section_outlines():
     arcade.draw_rectangle_outline(750, 450, 300, 300, arcade.color.BLACK)
     arcade.draw_rectangle_outline(1050, 450, 300, 300, arcade.color.BLACK)
 
-# Explore this
-
-#1: Pyramid from 10 - 54
-print("Number 1: Pyramid")
-n = 10
-for row in range (10):
-    for column in range(row):
-        print (n, end=" ")
-        n += 1
-    print()
-print()
-#2: Print a Box of N Size
-print("Number 2: Print a Box of N Size")
-n = input("How big do you want your box (enter a digit)?")
-n = int(n)
-for top in range (1):
-  for column in range (n*2):
-    print("o", end="")
-print()
-
-for middle in range(n*2-2):
-  for j in range (1):
-    print("o", end="")
-  for j in range (n*2-2):
-    print(" ", end="")
-  for j in range (1):
-    print("o", end="")
-  print()
-
-for bottom in range (1):
-  for column in range (n*2):
-    print("o", end="")
-
-#To here
-
-
-
-
-
 
 def draw_section_1():
     for row in range(30):
         for column in range(30):
-            x = 0  # Instead of zero, calculate the proper x location using 'column'
-            y = 0  # Instead of zero, calculate the proper y location using 'row'
+            x = column * 10 + 5
+            y = row * 10 + 5
             arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
 
 def draw_section_2():
-    # Below, replace "pass" with your code for the loop.
-    # Use the modulus operator and an if statement to select the color
-    # Don't loop from 30 to 60 to shift everything over, just add 300 to x.
-    pass
-
+    for row in range(30):
+        for column in range(30):
+            x = column * 10 + 305
+            y = row * 10 + 5
+            if column % 2 == 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+            else:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
 
 def draw_section_3():
-    # Use the modulus operator and an if/else statement to select the color.
-    # Don't use multiple 'if' statements.
-    pass
-
+    for row in range(30):
+        for column in range(30):
+            x = column * 10 + 605
+            y = row * 10 + 5
+            if row % 2 == 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+            else:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
 
 def draw_section_4():
-    # Use the modulus operator and just one 'if' statement to select the color.
-    pass
-
+    for row in range(30):
+        for column in range(30):
+            x = column * 10 + 905
+            y = row * 10 + 5
+            if column % 2 == 0 and row % 2 == 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+            else:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
 
 def draw_section_5():
-    # Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead.
-    pass
-
+    for column in range(30):
+        for row in range(column + 1):
+            x = column * 10 + 5
+            y =row * 10 + 305
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
 def draw_section_6():
-    pass
-
+    for row in range(30):
+        for column in range(30 - row):
+            x = column * 10 + 305
+            y = row * 10 + 305
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
 def draw_section_7():
-    pass
+    for row in range(30):
+        for column in range(row + 1):
+            x = column * 10 + 605
+            y = row * 10 + 305
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
 
 def draw_section_8():
-    pass
-
+    for row in range(30):
+        for column in range(row + 1):
+            x = (29 - column) * 10 + 905
+            y = row * 10 + 305
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
 def main():
     # Create a window
