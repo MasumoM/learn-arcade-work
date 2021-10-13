f6e3f1226f619d3731ad0061f0148e58f4309977
Masumo Mwenyi <masumo.mwenyi@my.simpson.edu>
2021-10-13 12:35:47 -0500

    Finished lab 6 assignment

--- Testing/test.py
+++ Testing/test.py
@@ -1,23 +1,73 @@
-class Boat():
-    def __init__(self):
-        self.tonnage = 0
-        self.name = ""
-        self.is_docked = True
+import arcade
+import random
 
-    def dock(self):
-        if self.is_docked:
-            print("You are already docked.")
-        else:
-            self.is_docked = True
-            print("Docking")
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
 
-    def undock(self):
-        if not self.is_docked:
-            print("You aren't docked.")
-        else:
-            self.is_docked = False
-            print("Undocking")
+class Ball:
+    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
+        """ Constructor. """
 
-class Submarine(Boat):
-    def submerge(self):
-        print("Submerge!")
+        # Take the parameters of the init function above, and create instance variables out of them.
+        self.position_x = position_x
+        self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
+        self.radius = radius
+        self.color = color
+
+    def draw(self):
+        arcade.draw_circle_filled(self.position_x,
+                                  self.position_y,
+                                  self.radius,
+                                  self.color)
+
+    def update(self):
+        self.position_y += self.change_y
+        self.position_x += self.change_x
+
+        if self.position_x < self.radius:
+            self.change_x *= -1
+        if self.position_x > SCREEN_WIDTH - self.radius:
+            self.change_x *= -1
+        if self.position_y < self.radius:
+            self.change_y *= -1
+        if self.position_y > SCREEN_HEIGHT - self.radius:
+            self.change_y *= -1
+
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.ASH_GREY)
+        self.ball_list = []
+
+        ball = Ball(50, 50, 3, 3, 15, arcade.color.AUBURN)
+        self.ball_list.append(ball)
+
+        ball = Ball(100, 150, 2, 3, 15, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+        self.ball_list.append(ball)
+
+        ball = Ball(150, 250, -3, -1, 15, arcade.color.FOREST_GREEN)
+        self.ball_list.append(ball)
+
+    def on_draw(self):
+        arcade.start_render()
+        for ball in self.ball_list:
+            ball.draw()
+
+    def update(self, delta_time):
+        for ball in self.ball_list:
+            ball.update()
+
+def main():
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
+
+    def main():
+        window = MyGame(640, 480, "Drawing Example")
+
+        arcade.run()
+
+    main()
