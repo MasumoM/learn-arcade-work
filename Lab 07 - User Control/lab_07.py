a14bd7f45d831adad1e89e48f41ccf84f4f169c6
Masumo Mwenyi <masumo.mwenyi@my.simpson.edu>
2021-10-28 14:36:29 -0500

    Lab seven for submisssion

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -9,8 +9,6 @@ DEAD_ZONE = 0.02
 class Ball:
     def __init__(self, position_x, position_y, change_x, change_y, radius, color):
 
-        # Take the parameters of the init function above,
-        # and create instance variables out of them.
         self.position_x = position_x
         self.position_y = position_y
         self.change_x = change_x
@@ -22,12 +20,9 @@ class Ball:
 
         def __init__(self, width, height):
             super().__init__(width, height, "Trigger Sound With Key")
-
-            # Load the sound when the application starts
             self.laser_sound = arcade.load_sound("laser.ogg")
 
         def on_key_press(self, key, modifiers):
-            # If the user hits  the space bar, play the sound that we loaded
             if key == arcade.key.SPACE:
                 arcade.play_sound(self.laser_sound)
 
@@ -42,7 +37,6 @@ class Ball:
         self.position_y += self.change_y
         self.position_x += self.change_x
 
-        # See if the ball hit the edge of the screen. If so, change direction
         if self.position_x < self.radius:
             self.position_x = self.radius
 
@@ -59,7 +53,6 @@ class Ball:
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
 
-        # Call the parent class's init function
         super().__init__(width, height, title)
         self.set_mouse_visible(False)
 
@@ -86,23 +79,6 @@ class MyGame(arcade.Window):
     def update(self, delta_time):
         self.ball.update()
 
-        def on_key_press(self, key, modifiers):
-            if key == arcade.key.LEFT:
-                self.ball.change_x = -MOVEMENT_SPEED
-            elif key == arcade.key.RIGHT:
-                self.ball.change_x = MOVEMENT_SPEED
-            elif key == arcade.key.UP:
-                self.ball.change_y = MOVEMENT_SPEED
-            elif key == arcade.key.DOWN:
-                self.ball.change_y = -MOVEMENT_SPEED
-
-        def on_key_release(self, key, modifiers):
-            """ Called whenever a user releases a key. """
-            if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-                self.ball.change_x = 0
-            elif key == arcade.key.UP or key == arcade.key.DOWN:
-                self.ball.change_y = 0
-        # Update the position according to the game controller
         if self.joystick:
             # "dead zone"
             if abs(self.joystick.x) < DEAD_ZONE:
